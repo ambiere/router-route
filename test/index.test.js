31ebdf7c74ba04660b074f60694d1c8d6dc37235
zhid0399123 <zhid0399123@gmail.com>
2024-06-23 20:24:47 +0300

    test file
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

---
+++ test/index.test.js
@@ -0,0 +1,24 @@
+import { test, assert } from 'vitest'
+import RrRoute from '../src/index.js'
+import customRouteObject from './fixture/customRouteObject'
+import standardRouteObject from './fixture/standardRouteObject'
+
+const it = test.extend({
+  routeMap: customRouteObject,
+  routeObject: standardRouteObject
+})
+
+it('should parse custom route object to a react-router compatible route object',
+  function testRrRoute ({ routeMap, routeObject }) {
+    const rrRoute = new RrRoute(routeMap)
+    const rrRouteObject = rrRoute.routeObject()
+
+    assert.isOk(rrRouteObject, 'returned route object')
+    assert.isArray(rrRouteObject)
+    assert.equal(
+      JSON.stringify(rrRouteObject),
+      JSON.stringify(routeObject),
+      'Returned react-router compatible route object'
+    )
+  }
+)
