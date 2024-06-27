7a84dd53fa9a83156ecbeb635888b60b3b24f0de
zhid0399123 <zhid0399123@gmail.com>
2024-06-27 17:56:29 +0300

    update test file

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,5 @@
-// import fs from 'node:fs'
 import { test, assert } from 'vitest'
-import RrRoute from '../src/index.js'
+import RouterRoute from '../src/index.js'
 import customRouteObject from './fixture/customRouteObject'
 import standardRouteObject from './fixture/standardRouteObject'
 
@@ -10,14 +9,14 @@ const it = test.extend({
 })
 
 it('should parse custom route object to a react-router compatible route object',
-  function testRrRoute({ routeMap, routeObject }) {
-    const rrRoute = new RrRoute(routeMap)
-    const rrRouteObject = rrRoute.routeObject()
+  function testRouterRoute ({ routeMap, routeObject }) {
+    const routerRoute = new RouterRoute(routeMap)
+    const routerRouteObject = routerRoute.routeObject()
 
-    assert.isOk(rrRouteObject, 'returned route object')
-    assert.isArray(rrRouteObject)
+    assert.isOk(routerRouteObject, 'returned route object')
+    assert.isArray(routerRouteObject)
     assert.equal(
-      JSON.stringify(rrRouteObject),
+      JSON.stringify(routerRouteObject),
       JSON.stringify(routeObject),
       'Returned react-router compatible route object'
     )
